3f188129e0897635ff197663144b21e1968ab6ce
amos <anton.khazan@gmail.com>
2023-11-25 06:09:31 +0200

    use the same core functions for both types of arrays

--- emulate-arrays.sh
+++ emulate-arrays.sh
@@ -12,17 +12,59 @@
 declare_i_arr() {
 	[ $# -lt 1 ] && { echo "declare_i_arr: Error: not enough arguments." >&2; return 1; }
 	___arr="$1"; shift
-	___values=""
+	___values=""; ___idex=0
 
 	if [ -n "$*" ]; then
 		for ___val in "$@"; do
-			___values="${___values}${__delim}$___val"
+			___index=$((___index+1))
+			___values="${___values}${__delim1}$___index $___val"
 		done
-		___values="${___values#"${__delim}"}"
+		___values="${___values#"${__delim1}"}"
 	fi
 
 	eval "emu_i_$___arr=\"$___values\""
-	unset ___arr ___values ___val
+	unset ___arr ___values ___val ___index
+	return 0
+}
+
+___set_arr_el() {
+	___arr="$1"; ___new_key="$2"; __new_val="$3"; __new_pairs=""
+
+	eval "___pairs=\"\$emu_${___arr_type}_${___arr}\""
+	IFS_OLD="$IFS"
+	IFS="${__delim1}"
+	# shellcheck disable=SC2154
+	for ___pair in $___pairs; do
+		___key="${___pair%% *}"
+		[ "$___new_key" != "$___key" ] && __new_pairs="${__new_pairs}${__delim1}${___pair}"
+	done
+	IFS="$IFS_OLD"
+
+	[ -n "$__new_val" ] && __new_pairs="${__new_pairs}${__delim1}${___new_key} ${__new_val}"
+	__new_pairs="${__new_pairs#"${__delim1}"}"
+	eval "emu_${___arr_type}_$___arr=\"$__new_pairs\""
+
+	unset ___arr __new_pair __new_key ___pairs ___pair ___key __new_pairs
+	return 0
+}
+
+___get_arr_el() {
+	___val=""; ___arr="$1"; ___key="$2"
+
+	eval "___pairs=\"\$emu_${___arr_type}_${___arr}\""
+
+	IFS_OLD="$IFS"
+	IFS="${__delim1}"
+	# shellcheck disable=SC2154
+	for ___pair in $___pairs; do
+		___arr_key="${___pair%% *}"
+		[ "$___arr_key" = "$___key" ] && ___val="${___pair#* }"
+	done
+	IFS="$IFS_OLD"
+
+	printf '%s\n' "$___val"
+	unset ___val ___arr ___key ___pairs ___pair ___arr_key
+
 	return 0
 }
 
@@ -34,101 +76,11 @@ declare_i_arr() {
 get_i_arr_el() {
 	[ $# -lt 2 ] && { echo "get_i_arr_el: Error: not enough arguments." >&2; return 1; }
 	[ $# -gt 2 ] && { echo "get_i_arr_el: Error: I only accept 2 arguments." >&2; return 1; }
-	___res=""; ___arr="$1"; ___index="$2"
+	___arr_type="i"
 
-	case "$___index" in ''|*[!0-9]*) echo "get_i_arr_el: Error: no index specified or '$___index' is not a positive integer." >&2; return 1 ;; esac
-	[ "$___index" -lt 1 ] && { echo "get_i_arr_el: Error: invalid index '$___index'." >&2; return 1; }
-
-	eval "___values=\"\$emu_i_$___arr\""
-
-	___i=0
-	IFS_OLD="$IFS"
-	IFS="${__delim}"
-	for ___val in $___values; do
-		___i=$((___i+1))
-		[ $___i -eq "$___index" ] && { ___res="$___val"; break; }
-	done
-	IFS="$IFS_OLD"
-
-	printf '%s\n' "$___res"
-	unset ___arr ___values ___val ___res ___index ___i
-
-	return 0
-}
-
-# set a value in an emulated indexed array
-# array contents are stored in a variable with the same name as the 'array' but with 'emu_i_' prefix
-# 1 - array name
-# 2 - index
-# 3 - value (if no value, unsets the index)
-# no additional arguments are allowed
-set_i_arr_el() {
-	[ $# -lt 2 ] && { echo "set_i_arr_el: Error: not enough arguments." >&2; return 1; }
-	[ $# -gt 3 ] && { echo "set_i_arr_el: Error: I only accept up to 3 arguments." >&2; return 1; }
-	___arr="$1"; ___index="$2"; __new_val="$3"; __new_values=""
-
-	case "$___index" in ''|*[!0-9]*) echo "set_i_arr_el: Error: no index specified or '$___index' is not a positive integer." >&2; return 1 ;; esac
-	[ "$___index" -lt 1 ] && { echo "set_i_arr_el: Error: invalid index '$___index'." >&2; return 1; }
-
-	eval "___values=\"\$emu_i_$___arr\""
-	__i=0
-	IFS_OLD="$IFS"
-	IFS="$__delim"
-	for ___val in $___values; do
-		__i=$((__i+1))
-		[ $__i -eq "$___index" ] && ___val="$__new_val"
-		__new_values="${__new_values}${__delim}$___val"
-	done
-	IFS="$IFS_OLD"
-
-	if [ $__i -lt "$___index" ]; then
-		# shellcheck disable=SC2034
-		for __j in $(seq $__i $((___index-2)) ); do
-			__new_values="${__new_values}${__delim}"
-		done
-		__new_values="${__new_values}${__delim}${__new_val}"
-	fi
-	__new_values="${__new_values#"${__delim}"}"
-
-	eval "emu_i_$___arr=\"$__new_values\""
-
-	unset ___arr ___values __new_values ___val ___index __i __j
-
-	return 0
-}
-
-# set a key=value pair in an emulated associative array
-# array contents are stored in a variable with the same name as the 'array' but with 'emu_a_' prefix
-# '#' is used as a delimiter
-# 1st arg - array name
-# 2nd arg - 'key=value' pair
-set_a_arr_el() {
-	[ $# -lt 2 ] && { echo "set_a_arr_el: Error: not enough arguments." >&2; return 1; }
-	[ $# -gt 2 ] && { echo "set_a_arr_el: Error: I only accept 2 arguments." >&2; return 1; }
-	___arr="$1"; __new_pair="$2"; __new_pairs=""
-
-	case "$__new_pair" in *=*) ;; *) echo "set_a_arr_el: Error: '$__new_pair' is not a 'key=value' pair." >&2; return 1 ;; esac
-	__new_key="${__new_pair%%=*}"
-	[ -z "$__new_key" ] && { echo "set_a_arr_el: Error: empty value provided for key in input '$__new_pair'." >&2; return 1; }
-	__new_val="${__new_pair#*=}"
-	eval "___pairs=\"\$emu_a_$___arr\""
-
-	IFS_OLD="$IFS"
-	IFS="${__delim}"
-	# shellcheck disable=SC2154
-	for ___pair in $___pairs; do
-		___key="${___pair%%=*}"
-		[ "$__new_key" != "$___key" ] && __new_pairs="${__new_pairs}${__delim}$___pair"
-	done
-	IFS="$IFS_OLD"
-
-	[ -n "$__new_val" ] && __new_pairs="${__new_pairs}${__delim}$__new_pair"
-	__new_pairs="${__new_pairs#"${__delim}"}"
-
-	eval "emu_a_$___arr=\"$__new_pairs\""
-
-	unset ___arr __new_pair __new_key ___pairs ___pair ___key __new_pairs
-	return 0
+	case "$2" in ''|*[!0-9]*) echo "get_i_arr_el: Error: no index specified or '$2' is not a positive integer." >&2; return 1 ;; esac
+	[ "$2" -lt 1 ] && { echo "get_i_arr_el: Error: invalid index '$2'." >&2; return 1; }
+	___get_arr_el "$@"
 }
 
 # get a value from an emulated associative array
@@ -139,22 +91,44 @@ set_a_arr_el() {
 get_a_arr_el() {
 	[ $# -lt 2 ] && { echo "get_a_arr_el: Error: not enough arguments." >&2; return 1; }
 	[ $# -gt 2 ] && { echo "get_a_arr_el: Error: I only accept 2 arguments." >&2; return 1; }
-	___val=""; ___arr="$1"; ___key="$2"
+	___arr_type="a"
 
-	eval "___pairs=\"\$emu_a_$___arr\""
-
-	IFS_OLD="$IFS"
-	IFS="${__delim}"
-	for ___pair in $___pairs; do
-		___arr_key="${___pair%%=*}"
-		[ "$___arr_key" = "$___key" ] && ___val="${___pair#*=}"
-	done
-	IFS="$IFS_OLD"
-
-	printf '%s\n' "$___val"
-	unset ___val ___arr ___key ___pairs ___pair ___arr_key
-
-	return 0
+	___get_arr_el "$@"
 }
 
-__delim="$(printf '\37')"
+# set a value in an emulated indexed array
+# array contents are stored in a variable with the same name as the 'array' but with 'emu_i_' prefix
+# 1 - array name
+# 2 - index
+# 3 - value (if no value, unsets the index)
+# no additional arguments are allowed
+set_i_arr_el() {
+	[ $# -lt 2 ] && { echo "set_i_arr_el: Error: not enough arguments." >&2; return 1; }
+	[ $# -gt 3 ] && { echo "set_i_arr_el: Error: I only accept 2 arguments." >&2; return 1; }
+	___new_index=$2; ___arr_type="i"
+
+	case $___new_index in ''|0|*[!0-9]*) echo "get_i_arr_el: Error: no index specified or '$___new_index' is not a positive integer." >&2; return 1 ;; esac
+
+	___set_arr_el "$@"
+}
+
+# set a key=value pair in an emulated associative array
+# array contents are stored in a variable with the same name as the 'array' but with 'emu_a_' prefix
+# '#' is used as a delimiter
+# 1st arg - array name
+# 2nd arg - 'key=value' pair
+set_a_arr_el() {
+	[ $# -lt 2 ] && { echo "set_a_arr_el: Error: not enough arguments." >&2; return 1; }
+	[ $# -gt 2 ] && { echo "set_a_arr_el: Error: I only accept 2 arguments." >&2; return 1; }
+	__new_pair="$2"; ___arr_type="a"
+
+	case "$__new_pair" in *=*) ;; *) echo "set_a_arr_el: Error: '$__new_pair' is not a 'key=value' pair." >&2; return 1 ;; esac
+	__new_key="${__new_pair%%=*}"
+	__new_val="${__new_pair#*=}"
+	[ -z "$__new_key" ] && { echo "set_a_arr_el: Error: empty value provided for key in input '$__new_pair'." >&2; return 1; }
+
+	___set_arr_el "$1" "$__new_key" "$__new_val"
+}
+
+
+__delim1="$(printf '\37')"
