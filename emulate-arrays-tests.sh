538c6d19358187410973874374b3e44f6e8ac8c7
amos <anton.khazan@gmail.com>
2023-11-25 03:36:25 +0200

    multiple enhancements

--- emulate-arrays-tests.sh
+++ emulate-arrays-tests.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-# shellcheck disable=SC2154,SC2034
+# shellcheck disable=SC2154,SC2034,SC2120
 
 # emulate-arrays-tests.sh
 
 #### Initial setup
 export LC_ALL=C
-
 me=$(basename "$0")
 
 script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
@@ -14,380 +13,119 @@ script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 . "$script_dir/emulate-arrays.sh" || { echo "$me: Error: Can't source '$script_dir/trim-subnet.sh'." >&2; exit 1; }
 
 
-declare_wrong_i_arr() {
-	# shellcheck disable=SC2119
-	declare_i_arr 2>/dev/null; rv=$?
-	[ $rv -ne 1 ] && echo "declare_wrong_i_arr failed"
-	unset rv
-}
-
-test_i_arr() {
-
+run_test() {
 # Test sets use '@' as a column delimiter
 # 1st 1 or more lines format: 'declare' function test
 # 1st col: 'declare' function with input args, 2nd col: return code
 
 # further lines format is for 'get' function tests
-# 1st col: 'get function input args, 2nd col: expected value, 3rd col: expected return code
-
-tests_1="
-declare_i_arr arr@0
-@@1
-arr@@1
-arr 0@@1
-arr 1@@0
-arr 2@@0
-arr 1 2@@1
-arr a@@1
-arr 1 a@@1
-arr a 1@@1
-"
-tests_2="
-declare_i_arr arr abc@0
-@@1
-arr@@1
-arr 0@@1
-arr 1@abc@0
-arr 2@@0
-arr 3@@0
-arr 1 2@@1
-arr a@@1
-arr 1 a@@1
-arr a 1@@1
-"
-tests_3="
-declare_i_arr arr abc 123@0
-@@1
-arr@@1
-arr 0@@1
-arr 1@abc@0
-arr 2@123@0
-arr 3@@0
-arr 1 2@@1
-arr a@@1
-arr 1 a@@1
-arr a 1@@1
-"
-tests_4="
-declare_i_arr arr abc 123 a@0
-@@1
-arr@@1
-arr 0@@1
-arr 1@abc@0
-arr 2@123@0
-arr 3@a@0
-arr 1 2@@1
-arr a@@1
-arr 1 a@@1
-arr a 1@@1
-"
-tests_5="
-declare_i_arr arr abc 123 \"a bcd 123\"@0
-@@1
-arr@@1
-arr 0@@1
-arr 1@abc@0
-arr 2@123@0
-arr 3@a bcd 123@0
-arr 4@@0
-arr 1 2@@1
-arr a@@1
-arr 1 a@@1
-arr a 1@@1
-"
+# 1st col: 'get function call with input args, 2nd col: expected value, 3rd col: expected return code
 
 	id="$1"
-	eval "tests=\"\$tests_$id\""
+	var_name="$2"
+	eval "test=\"\$test_$id\""
+	echo; echo "Test id: $id."
 
 	# remove extra whitespaces, tabs and newlines
-	tests="$(printf "%s" "$tests" | awk '$0=="" {next} {$1=$1};1')"
+	test="$(printf "%s" "$test" | awk '$0=="" {next} {$1=$1};1')"
 
-	# separate 'declare' lines (header) from 'get' commands
-	header="$(printf '%s' "$tests" | grep 'declare_i_arr')"
+	## separate 'declare' lines (header) from 'get' commands
+	# get the header lines
+	header="$(printf '%s\n' "$test" | sed -n -e /"\[header\]"/\{:1 -e n\;/"\[\/header\]"/q\;p\;b1 -e \})"
 	header_lines_cnt="$(printf '%s\n' "$header" | wc -l)"
-	echo "header lines: $header_lines_cnt"
 
-	tests="$(printf '%s' "$tests" | tail -n+"$((header_lines_cnt+1))")"
-	lines_cnt="$(printf '%s\n' "$tests" | wc -l)"
+	# get the test lines
+	test="$(printf '%s' "$test" | tail -n+"$((header_lines_cnt+3))")"
+	lines_cnt="$(printf '%s\n' "$test" | wc -l)"
 
 	# execute 'declare' commands
 	for i in $(seq 1 "$header_lines_cnt"); do
 		header_line="$(printf '%s\n' "$header" | awk "NR==$i")"
 		test_command="${header_line%@*}"
-		exp_rv="${header_line#*@}"
+		expected_rv="${header_line#*@}"
 		echo "test_command: '$test_command'"
-		eval "$test_command" 2>/dev/null; rv=$?
-		[ "$rv" != "$exp_rv" ] && echo "Error: test '$id', expected rv: '$exp_rv', got rv: '$rv'" >&2
+
+		if [ -z "$print_errors" ]; then eval "$test_command" 2>/dev/null; rv=$?
+		else eval "$test_command"; rv=$?
+		fi
+		
+		[ "$rv" != "$expected_rv" ] && echo "Error: test '$id', expected rv: '$expected_rv', got rv: '$rv'" >&2
 	done
 
 	# execute 'get' commands
 	for i in $(seq 1 "$lines_cnt" ); do
-		line="$(printf '%s\n' "$tests" | awk "NR==$i")"
-		in_args="${line%%@*}"
+		line="$(printf '%s\n' "$test" | awk "NR==$i")"
+		test_command="${line%%@*}"
 		other_stuff="${line#*@}"
 		expected_val="${other_stuff%@*}"
 		expected_rv="${other_stuff#*@}"
 
 		# shellcheck disable=SC2086
-		val="$(get_i_arr_el $in_args 2>/dev/null)"; rv=$?
+		if [ -z "$print_errors" ]; then val="$($test_command 2>/dev/null)"; rv=$?
+		else val="$($test_command)"; rv=$?
+		fi
+
 		[ "$val" != "$expected_val" ] && echo "Error: test '$id', test line: '$line', expected val: '$expected_val', got val: '$val'" >&2
 		[ "$rv" != "$expected_rv" ] && echo "Error: test '$id', test line: '$line', expected rv: '$expected_rv', got rv: '$rv'" >&2
+		printf '%s' "."
 	done
-	unset emu_i_arr
-	unset declare_str exp_declare_rv in_args other_stuff expected_val expected_rv val
+	printf '\n'
+	unset "$var_name"
+	unset test_command other_stuff expected_val expected_rv val
 }
 
-declare_wrong_i_arr
 
-echo "testing 'declare_i_arr' and 'get_i_arr_el'..."
-for j in $(seq 1 5); do
-	test_i_arr "$j"
-done
-
-
-
-
-
-wrong_a_arr() {
-	# shellcheck disable=SC2119
-	set_a_arr_el 2>/dev/null; rv=$?
-	[ $rv -ne 1 ] && echo "Error: set_wrong_arr failed. Expected rv: 1, got rv: '$rv'"
+run_test_a_arr() {
+	. "$script_dir/tests-set_a_arr.list"
+	echo
+	echo "*** testing 'set_a_arr_el' and 'get_a_arr_el'... ***"
+	next_test="start"
+	j=1
+	maxtests=${1:-100}
+	while [ -n "$next_test" ] && [ "$j" -le "$maxtests" ]; do
+		run_test "$j" emu_a_arr
+		j=$((j+1))
+		eval "next_test=\"\$test_$j"\"
+	done
 }
 
-test_a_arr() {
-
-# Test sets use '@' as a column delimiter
-# 1st 1 or more lines format: 'set' function test
-# 1st col: 'set' function call with input args, 2nd col: expected return code
-
-# further lines format is for 'get' function tests
-# 1st col: 'get' function input args, 2nd col: expected value, 3rd col: expected return code
-
-tests_1="
-set_a_arr_el arr@1
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abc@@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_2="
-set_a_arr_el arr abc@1
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abc@@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_3="
-set_a_arr_el arr abc 123@1
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abc@@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_4="
-set_a_arr_el arr abc 123=a@1
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abc@@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_5="
-set_a_arr_el arr 123=a abc@1
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abc@@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_6="
-set_a_arr_el arr 123=a abc=d@1
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abc@@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_7="
-set_a_arr_el arr 123=abcd@0
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abcd@@0
-arr 123@abcd@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_8="
-set_a_arr_el arr 123=abcd@0
-set_a_arr_el arr abcd=123@0
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr abcd@123@0
-arr 123@abcd@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_9="
-set_a_arr_el arr 123=abcd@0
-set_a_arr_el arr abcd=123@0
-set_a_arr_el arr aghk=a123@0
-set_a_arr_el arr 1ghk=b123@0
-@@1
-arr 1ghk@b123@0
-arr@@1
-arr 0@@0
-arr 1@@0
-arr aghk@a123@0
-arr 1 2@@1
-arr a@@0
-arr 123@abcd@0
-arr abcd@123@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_10="
-set_a_arr_el arr 123=abcd@0
-set_a_arr_el arr abcd=123@0
-set_a_arr_el arr abcd=jjjj@0
-set_a_arr_el arr 123=@0
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr 123@@0
-arr abcd@jjjj@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_11="
-set_a_arr_el arr 123=abcd@0
-set_a_arr_el arr abcd=123@0
-set_a_arr_el arr abcd=jjjj@0
-set_a_arr_el arr 123=@0
-set_a_arr_el arr abcd=@0
-@@1
-arr@@1
-arr 0@@0
-arr 1@@0
-arr 1 2@@1
-arr a@@0
-arr 123@@0
-arr abcd@@0
-arr 1 a@@1
-arr a 1@@1
-"
-tests_12="
-set_a_arr_el arr 123=\"abcd efgh 123\"@0
-set_a_arr_el arr abcd=123@0
-set_a_arr_el arr aghk=a123@0
-set_a_arr_el arr 1ghk=b123@0
-@@1
-arr 1ghk@b123@0
-arr@@1
-arr 0@@0
-arr 1@@0
-arr aghk@a123@0
-arr 1 2@@1
-arr a@@0
-arr 123@abcd efgh 123@0
-arr abcd@123@0
-arr 1 a@@1
-arr a 1@@1
-"
-
-	id="$1"
-	eval "tests=\"\$tests_$id\""
-
-	# remove extra whitespaces, tabs and newlines
-	tests="$(printf "%s" "$tests" | awk '$0=="" {next} {$1=$1};1')"
-
-	# separate 'set' lines (header) from 'get' commands
-	header="$(printf '%s' "$tests" | grep 'set_a_arr_el')"
-	header_lines_cnt="$(printf '%s\n' "$header" | wc -l)"
-	echo "header lines: $header_lines_cnt"
-
-	tests="$(printf '%s' "$tests" | tail -n+"$((header_lines_cnt+1))")"
-	tests_lines_cnt="$(printf '%s\n' "$tests" | wc -l)"
-
-	# execute 'set' commands
-	for i in $(seq 1 "$header_lines_cnt"); do
-		header_line="$(printf '%s\n' "$header" | awk "NR==$i")"
-		test_command="${header_line%@*}"
-		exp_rv="${header_line#*@}"
-		echo "test_command: '$test_command'"
-		eval "$test_command" 2>/dev/null; rv=$?
-		[ "$rv" != "$exp_rv" ] && echo "Error: test '$id', expected rv: '$exp_rv', got rv: '$rv'" >&2
+run_test_set_i_arr() {
+	. "$script_dir/tests-set_i_arr.list"
+	echo
+	echo "*** testing 'set_i_arr_el' and 'get_i_arr_el'... ***"
+	next_test="start"
+	j=1
+	maxtests=${1:-100}
+	while [ -n "$next_test" ] && [ "$j" -le "$maxtests" ]; do
+		run_test "$j" emu_i_arr
+		j=$((j+1))
+		eval "next_test=\"\$test_$j"\"
 	done
-
-	# execute 'get' commands
-	for i in $(seq 1 "$tests_lines_cnt" ); do
-		line="$(printf '%s\n' "$tests" | awk "NR==$i")"
-		in_args="${line%%@*}"
-		other_stuff="${line#*@}"
-		expected_val="${other_stuff%@*}"
-		expected_rv="${other_stuff#*@}"
-
-		# shellcheck disable=SC2086
-		val="$(get_a_arr_el $in_args 2>/dev/null)"; rv=$?
-		[ "$val" != "$expected_val" ] && echo "Error: test '$id', test line: '$line', expected val: '$expected_val', got val: '$val'" >&2
-		[ "$rv" != "$expected_rv" ] && echo "Error: test '$id', test line: '$line', expected rv: '$expected_rv', got rv: '$rv'" >&2
-	done
-	unset emu_a_arr
 }
 
-echo
-wrong_a_arr
+run_test_declare_i_arr() {
+	. "$script_dir/tests-declare_i_arr.list"
+	echo
+	echo "*** testing 'declare_i_arr' and 'get_i_arr_el'... ***"
+	next_test="start"
 
-echo
-echo "testing 'set_a_arr_el' and 'get_a_arr_el'..."
-for j in $(seq 1 12); do
-	test_a_arr "$j"
-done
+	j=1
+	maxtests=${1:-100}
+	while [ -n "$next_test" ] && [ "$j" -le "$maxtests" ]; do
+		run_test "$j" emu_i_arr
+		j=$((j+1))
+		eval "next_test=\"\$test_$j"\"
+	done
+}
 
 
+#print_stderr=true
 
+run_test_declare_i_arr
+run_test_set_i_arr
+run_test_a_arr
 
-# declare_i_arr ahrttr_asdfasdfasdf asdfasdfasdfasdfasdfasfasfs123 r t aasdf asdfasdf asdfasdfsdfs asdfsdfsdfdsf asdfsdf 1>/dev/null
-# for i in $(seq 1 1000); do
-# 	get_i_arr_el ahrttr_asdfasdfasdf 7 1>/dev/null
+# for i in $(seq 1 100); do
+# 	declare_i_arr abc def ghk 123456 blah bruh "$i"
+# 	test="$(get_i_arr_el abc $i)"
 # done
-
-# declare_i_arr arr1 a b "1 2 3"
-# get_i_arr_el arr1 3
