70465e066b50fc472895ac8a459fa39fd1115d07
amos <anton.khazan@gmail.com>
2023-11-26 16:09:10 +0200

    added some comments

--- emulate-arrays-tests.sh
+++ emulate-arrays-tests.sh
@@ -161,12 +161,19 @@ run_test_declare_i_arr() {
 newline="
 "
 
+# To print errors returned by the functions under test, uncomment the following line
+# Some of the test units intentionally induce errors, so expect an error spam in the console
 #print_stderr=true
 
 err_num=0
+
+# To only run a specific test set, comment out some of the following lines starting with run_test_
+# To limit to sepcific test units, use this format run_test_* [first_test_number] [last_test_number]
+# For example, 'run_test_a_arr 5 8' will run test units 5 through 8
 run_test_declare_i_arr
 run_test_set_i_arr
 run_test_a_arr
+
 printf '\n%s\n' "Total errors: $err_num."
 
 ### Performance tests
