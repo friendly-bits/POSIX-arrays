02a82f6046cc8d2314fd1bb2ca8eea290d5d84d9
amos <anton.khazan@gmail.com>
2023-11-26 17:06:04 +0200

    code reorganization and cleanup

--- emulate-arrays-tests.sh
+++ emulate-arrays-tests.sh
@@ -23,136 +23,129 @@ run_test() {
 # further lines format is for 'get' function tests
 # 1st col: 'get' function call with input args, 2nd col: expected value, 3rd col: expected return code
 
-	id="$1"
-	arr_type="$2"
-	test="$3"
-	echo; echo "Test id: $id."
+	arr_type="$1"
+	test_file="$2"
+	first_test_num=${3:-1}
+	last_test_num=${4:-100}
 
-	# remove extra whitespaces, tabs and newlines
-	test="$(printf "%s" "$test" | awk '$0=="" {next} {$1=$1};1')"
+	# shellcheck disable=SC1090
+	. "$test_file"
 
-	## separate 'declare' lines (header) from 'get' commands
-	# get the header lines
-	header="$(printf '%s\n' "$test" | sed -n -e /"\[header\]"/\{:1 -e n\;/"\[\/header\]"/q\;p\;b1 -e \})"
-	header_lines_cnt="$(printf '%s\n' "$header" | wc -l)"
+	k="$first_test_num"
 
-	# get the test lines
-	test="$(printf '%s' "$test" | tail -n+"$((header_lines_cnt+3))")"
-	lines_cnt="$(printf '%s\n' "$test" | wc -l)"
+	# load the first test unit
+	eval "test_unit=\"\$test_$k\""
 
-	# execute 'declare' and 'set' commands
-	while [ -n "$header" ]; do
-		# get line/s for the next command
-		header_line="$(printf '%s\n' "$header" | \
-			sed -n -e /"_${arr_type}_arr"/\{:1 -e p\;n\;/"_${arr_type}_arr"/q\;b1 -e \})"
-		# remove next command line/s from the list
-		header="${header#"$header_line"}"; header="${header#?}"
-		# extract test specifics
-		test_command="${header_line%@*}"
-		expected_rv="${header_line#*@}"
-		echo "**header test_command: '$test_command'"
+	while [ -n "$test_unit" ] && [ "$k" -le "$last_test_num" ]; do
+		# gather test variables names to unset them later
+		test_var_names="test_$k $test_var_names"
 
-		# gather array names from the test to reset the variables in the end
-		arr_name="$(printf '%s' "$test_command" | head -n1 | cut -d' ' -f2)"
-		case "$arr_name" in *_arr_* ) ;; *) arr_names="${arr_name}${newline}${arr_names}"; esac
+		test_id="$k"
+		echo; echo "Test id: $test_id."
 
-		if [ -z "$print_stderr" ]; then eval "$test_command" 2>/dev/null; rv=$?
-		else eval "$test_command"; rv=$?
-		fi
-		
-		[ "$rv" != "$expected_rv" ] && {
-			echo "Error: test '$id', expected rv: '$expected_rv', got rv: '$rv'" >&2
-			err_num=$((err_num+1)); }
+		# remove extra whitespaces, tabs and newlines
+		test_unit="$(printf "%s" "$test_unit" | awk '$0=="" {next} {$1=$1};1')"
+
+		## separate 'declare' lines (header) from 'get' commands
+		# get the header lines
+		header_test_unit="$(printf '%s\n' "$test_unit" | \
+			sed -n -e /"\[header\]"/\{:1 -e n\;/"\[\/header\]"/q\;p\;b1 -e \})"
+		header_lines_cnt="$(printf '%s\n' "$header_test_unit" | wc -l)"
+
+		# get the main test lines
+		main_test_unit="$(printf '%s' "$test_unit" | tail -n+"$((header_lines_cnt+3))")"
+		main_lines_cnt="$(printf '%s\n' "$test_unit" | wc -l)"
+
+		# execute 'declare' and 'set' commands
+		while [ -n "$header_test_unit" ]; do
+			# get line/s for the next command
+			header_line="$(printf '%s\n' "$header_test_unit" | \
+				sed -n -e /"_${arr_type}_arr"/\{:1 -e p\;n\;/"_${arr_type}_arr"/q\;b1 -e \})"
+			# remove next command line/s from the list
+			header_test_unit="${header_test_unit#"$header_line"}"; header_test_unit="${header_test_unit#?}"
+			# extract test unit specifics
+			test_command="${header_line%@*}"
+			expected_rv="${header_line#*@}"
+			echo "**header test_command: '$test_command'"
+
+			# gather array names from the test to reset the variables in the end
+			arr_name="$(printf '%s' "$test_command" | head -n1 | cut -d' ' -f2)"
+			case "$arr_name" in *_arr_* ) ;; *) arr_names="${arr_name}${newline}${arr_names}"; esac
+
+			if [ -z "$print_stderr" ]; then eval "$test_command" 2>/dev/null; rv=$?
+			else eval "$test_command"; rv=$?
+			fi
+			
+			[ "$rv" != "$expected_rv" ] && {
+				echo "Error: test '$test_id', expected rv: '$expected_rv', got rv: '$rv'" >&2
+				err_num=$((err_num+1)); }
+		done
+
+		# execute the 'get' commands
+		while [ -n "$main_test_unit" ]; do
+			# get line/s for the next command
+			line="$(printf '%s\n' "$main_test_unit" | \
+				sed -n -e /"_${arr_type}_arr"/\{:1 -e p\;n\;/"_${arr_type}_arr"/q\;b1 -e \})"
+			# remove line/s for the next command from the list
+			main_test_unit="${main_test_unit#"$line"}"; main_test_unit="${main_test_unit#?}"
+			# extract test unit specifics
+			test_command="${line%%@*}"
+			other_stuff="${line#*@}"
+			expected_val="${other_stuff%@*}"
+			expected_rv="${other_stuff#*@}"
+
+			# shellcheck disable=SC2086
+			if [ -z "$print_stderr" ]; then val="$($test_command 2>/dev/null)"; rv=$?
+			else val="$($test_command)"; rv=$?
+			fi
+
+			[ "$val" != "$expected_val" ] && {
+				echo "Error: test '$test_id', test line: '$line', expected val: '$expected_val', got val: '$val'" >&2
+				err_num=$((err_num+1)); }
+			[ "$rv" != "$expected_rv" ] && {
+				echo "Error: test '$test_id', test line: '$line', expected rv: '$expected_rv', got rv: '$rv'" >&2
+				err_num=$((err_num+1)); }
+			printf '%s' "."
+		done
+		printf '\n'
+
+		# unset the variables holding the arrays
+		arr_names="$(printf '%s\n' "$arr_names" | sort -u)"
+		for arr_name in $arr_names; do
+			unset "emu_${arr_type}_${arr_name}" 2>/dev/null
+		done
+		unset test_command other_stuff expected_val expected_rv val arr_names
+
+		# load the next test unit
+		k=$((k+1))
+		eval "test_unit=\"\$test_$k\""
 	done
 
-	# execute the 'get' commands
-	while [ -n "$test" ]; do
-		# get line/s for the next command
-		line="$(printf '%s\n' "$test" | \
-			sed -n -e /"_${arr_type}_arr"/\{:1 -e p\;n\;/"_${arr_type}_arr"/q\;b1 -e \})"
-		# remove next command line/s from the list
-		test="${test#"$line"}"; test="${test#?}"
-		# extract test specifics
-		test_command="${line%%@*}"
-		other_stuff="${line#*@}"
-		expected_val="${other_stuff%@*}"
-		expected_rv="${other_stuff#*@}"
-
-		# shellcheck disable=SC2086
-		if [ -z "$print_stderr" ]; then val="$($test_command 2>/dev/null)"; rv=$?
-		else val="$($test_command)"; rv=$?
-		fi
-
-		[ "$val" != "$expected_val" ] && {
-			echo "Error: test '$id', test line: '$line', expected val: '$expected_val', got val: '$val'" >&2
-			err_num=$((err_num+1)); }
-		[ "$rv" != "$expected_rv" ] && {
-			echo "Error: test '$id', test line: '$line', expected rv: '$expected_rv', got rv: '$rv'" >&2
-			err_num=$((err_num+1)); }
-		printf '%s' "."
-	done
-	printf '\n'
-
-	arr_names="$(printf '%s\n' "$arr_names" | sort -u)"
-	for arr_name in $arr_names; do
-		unset "emu_${arr_type}_${arr_name}" 2>/dev/null
-	done
-	unset test_command other_stuff expected_val expected_rv val arr_names
+	# shellcheck disable=SC2086
+	# unset the variables holding the test units
+	unset $test_var_names; unset test_var_names
 }
 
 
 run_test_a_arr() {
-	. "$script_dir/tests-set_a_arr.list"
-	echo
-	echo "*** Testing 'set_a_arr_el' and 'get_a_arr_el'... ***"
-	first_test=${1:-1}
-	last_test=${2:-100}
-	j="$first_test"
-	test="$test_1"
-	while [ -n "$test" ] && [ "$j" -le "$last_test" ]; do
-		tests_list="test_$j $tests_list"
-		run_test "$j" a "$test"
-		j=$((j+1))
-		eval "test=\"\$test_$j\""
-	done
-	# shellcheck disable=SC2086
-	unset $tests_list; unset tests_list
+	first_test_num=$1; last_test_num=$2; arr_type="a"
+	test_file="$script_dir/tests-set_a_arr.list"
+	echo; echo "*** Testing 'set_a_arr_el' and 'get_a_arr_el'... ***"
+	run_test "$arr_type" "$test_file" "$first_test_num" "$last_test_num"
 }
 
 run_test_set_i_arr() {
-	. "$script_dir/tests-set_i_arr.list"
-	echo
-	echo "*** Testing 'set_i_arr_el' and 'get_i_arr_el'... ***"
-	first_test=${1:-1}
-	last_test=${2:-100}
-	j="$first_test"
-	test="$test_1"
-	while [ -n "$test" ] && [ "$j" -le "$last_test" ]; do
-		tests_list="test_$j $tests_list"
-		run_test "$j" i "$test"
-		j=$((j+1))
-		eval "test=\"\$test_$j\""
-	done
-	# shellcheck disable=SC2086
-	unset $tests_list; unset tests_list
+	first_test_num=$1; last_test_num=$2; arr_type="i"
+	test_file="$script_dir/tests-set_i_arr.list"
+	echo; echo "*** Testing 'set_i_arr_el' and 'get_i_arr_el'... ***"
+	run_test "$arr_type" "$test_file" "$first_test_num" "$last_test_num"
 }
 
 run_test_declare_i_arr() {
-	. "$script_dir/tests-declare_i_arr.list"
-	echo
-	echo "*** Testing 'declare_i_arr' and 'get_i_arr_el'... ***"
-	first_test=${1:-1}
-	last_test=${2:-100}
-	j="$first_test"
-	test="$test_1"
-	while [ -n "$test" ] && [ "$j" -le "$last_test" ]; do
-		tests_list="test_$j $tests_list"
-		run_test "$j" i "$test"
-		j=$((j+1))
-		eval "test=\"\$test_$j\""
-	done
-	# shellcheck disable=SC2086
-	unset $tests_list; unset tests_list
+	first_test_num=$1; last_test_num=$2; arr_type="i"
+	test_file="$script_dir/tests-declare_i_arr.list"
+	echo; echo "*** Testing 'declare_i_arr' and 'get_i_arr_el'... ***"
+	run_test "$arr_type" "$test_file" "$first_test_num" "$last_test_num"
 }
 
 
@@ -168,7 +161,7 @@ newline="
 err_num=0
 
 # To only run a specific test set, comment out some of the following lines starting with run_test_
-# To limit to sepcific test units, use this format run_test_* [first_test_number] [last_test_number]
+# To limit to sepcific test units, use this format run_test_* [first_test_num_number] [last_test_num_number]
 # For example, 'run_test_a_arr 5 8' will run test units 5 through 8
 run_test_declare_i_arr
 run_test_set_i_arr
