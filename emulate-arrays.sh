69d4e14ea1e2274e0803c85626a57c43e823e3bc
amos <anton.khazan@gmail.com>
2023-12-02 23:51:07 +0200

    rewrite with a much faster algorithm and additional functions

--- emulate-arrays.sh
+++ emulate-arrays.sh
@@ -7,25 +7,44 @@
 # array contents are stored in a variable with the same name as the 'array' but with 'emu_[x]_' prefix
 # where [x] is either 'a' for associative array or [i] for indexed array
 
-
 # declare an emulated indexed array while populating first elements
 # 1 - array name
 # all other args - array values
 declare_i_arr() {
 	[ $# -lt 1 ] && { echo "declare_i_arr: Error: not enough arguments." >&2; return 1; }
 	___arr_name="$1"; shift
-	___values=""; ___index=0
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "declare_i_arr: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+	___indices=""
 
+	clean_i_arr "$___arr_name"
+	___index=0
 	if [ -n "$*" ]; then
 		for ___val in "$@"; do
-			___values="${___values}${___emu_arr_delim}$___index${___pair_delim}$___val"
+			eval "___emu_i_${___arr_name}_${___index}=\"$___val\""
+			___indices="$___indices$___index$___emu_arr_delim"
 			___index=$((___index+1))
 		done
 	fi
 
-	eval "emu_i_$___arr_name=\"$___values\""
-	unset ___arr_name ___values ___val ___index
+	eval "___emu_i_${___arr_name}_indices=\"$___indices\""
+
+	unset ___val ___index ___indices
 	return 0
+}
+
+clean_i_arr() {
+	[ $# -ne 1 ] && { echo "clean_i_arr: Error: wrong number of arguments." >&2; return 1; }
+	___arr_name="$1"
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "clean_i_arr: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+
+	eval "___indices=\"\$___emu_i_${___arr_name}_indices\""
+	___indices_sorted="$(printf '%s' "$___indices" | tr "$___emu_arr_delim" '\n' | sort -n)"
+
+	for ___index in $___indices_sorted; do
+		unset "___emu_i_${___arr_name}_${___index}"
+	done
+	unset "___emu_i_${___arr_name}_indices"
+	unset ___indices ___indices_sorted ___index
 }
 
 # get all values from an emulated indexed array (sorted by index)
@@ -33,23 +52,128 @@ declare_i_arr() {
 # no additional arguments are allowed
 get_i_arr_all() {
 	[ $# -ne 1 ] && { echo "get_i_arr_all: Error: wrong number of arguments." >&2; return 1; }
+	___arr_name="$1"; ___all_indices=""
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "get_i_arr_all: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+
+	eval "___indices=\"\$___emu_i_${___arr_name}_indices\""
+	___indices_sorted="$(printf '%s' "$___indices" | tr "$___emu_arr_delim" '\n' | sort -u | sort -n)"
+	for ___index in $___indices_sorted; do
+		eval "___val=\"\$___emu_i_${___arr_name}_${___index}\""
+		[ -n "$___val" ] && { printf '%s\n' "$___val"; ___all_indices="$___all_indices$___index$___emu_arr_delim"; }
+	done
+	eval "___emu_i_${___arr_name}_indices=\"$(printf '%s' "$___all_indices" | tr '\n' "$___emu_arr_delim")\""
+
+	unset ___all_values ___indices ___indices_sorted ___index ___val ___all_indices
+	return 0
+}
+
+# get all indices from an emulated indexed array (sorted)
+# 1 - array name
+# no additional arguments are allowed
+get_i_arr_all_indices() {
+	[ $# -ne 1 ] && { echo "get_i_arr_all_indices: Error: wrong number of arguments." >&2; return 1; }
 	___arr_name="$1"
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "get_i_arr_all_indices: Error: invalid array name '$___arr_name'." >&2; return 1; esac
 
-	eval "___pairs=\"\$emu_i_${___arr_name}\""
-	# shellcheck disable=SC2154
-	___pairs_sorted="$(printf '%s' "$___pairs" | tr "${___emu_arr_delim}" '\n' | sort -n)"
-	___all_values="$(
-		IFS_OLD="$IFS"
-		IFS="$___newline"
-		for ___pair in $___pairs_sorted; do
-			printf '%s' "${___pair#*"${___pair_delim}"} "
+	eval "___indices=\"\$___emu_i_${___arr_name}_indices\""
+	___indices_sorted="$(printf '%s' "$___indices" | tr "$___emu_arr_delim" '\n' | sort -u | sort -n)"
+	___all_indices="$(
+		for ___index in $___indices_sorted; do
+			eval "___val=\"\$___emu_i_${___arr_name}_${___index}\""
+			[ -n "$___val" ] && printf '%s\n' "$___index"
 		done
-		IFS="$IFS_OLD"
 	)"
+	eval "___emu_i_${___arr_name}_indices=\"$(printf '%s' "$___all_indices" | tr '\n' "$___emu_arr_delim")\""
+	printf '%s' "$___all_indices"
 
-	printf '%s\n' "${___all_values%" "}"
-	unset ___all_values ___arr_name ___pairs_sorted
+	unset ___indices ___indices_sorted ___index ___all_indices
+	return 0
+}
 
+# set a value in an emulated indexed sparse array
+# 1 - array name
+# 2 - index
+# 3 - value (if no value, unsets the index)
+# no additional arguments are allowed
+set_i_arr_el() {
+	___arr_name="$1"; ___index="$2"; ___new_val="$3"
+	if [ $# -lt 2 ] || [ $# -gt 3 ]; then echo "set_i_arr_el: Error: Wrong number of arguments." >&2; return 1; fi
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "set_i_arr_el: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+	case "$___index" in *[!0-9]*) echo "set_i_arr_el: Error: '$___index' is not a nonnegative integer." >&2; return 1 ; esac
+
+	eval "___old_val=\"\$___emu_i_${___arr_name}_${___index}\""
+
+	if [ -z "$___old_val" ] && [ -n "$___new_val" ]; then
+		eval "___emu_i_${___arr_name}_indices=\"\${___emu_i_${___arr_name}_indices}${___index}${___emu_arr_delim}\""
+	fi
+
+	eval "___emu_i_${___arr_name}_${___index}=\"$___new_val\""
+
+	unset ___arr_name ___index ___new_val ___indices ___old_val
+	return 0
+}
+
+# get a value from an emulated indexed array
+# 1 - array name
+# 2 - index
+# no additional arguments are allowed
+get_i_arr_el() {
+	if [ $# -ne 2 ]; then echo "get_i_arr_el: Error: Wrong number of arguments." >&2; return 1; fi
+	___arr_name="$1"; ___index=$2
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "get_i_arr_el: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+	case "$___index" in *[!0-9]*) echo "get_i_arr_el: Error: no index specified or '$___index' is not a nonnegative integer." >&2; return 1; esac
+	eval "printf '%s\n' \"\$___emu_i_${___arr_name}_${___index}\""
+}
+
+
+
+
+# declare an emulated associative array while populating elements
+# 1 - array name
+# all other args - 'key=value' pairs
+declare_a_arr() {
+	[ $# -lt 1 ] && { echo "declare_a_arr: Error: not enough arguments." >&2; return 1; }
+	___arr_name="$1"; shift
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "declare_a_arr: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+	___keys=""
+
+	clean_a_arr "$___arr_name"
+	if [ -n "$*" ]; then
+		for ___pair in "$@"; do
+			case "$___pair" in *=*) ;; *) echo "declare_a_arr: Error: '$___pair' is not a 'key=value' pair." >&2; return 1 ;; esac
+			___key="${___pair%%=*}"
+			___val="${___pair#*=}"
+			case "$___key" in *[!A-Za-z0-9_]*) echo "declare_a_arr: Error: invalid key '$___key'." >&2; return 1; esac
+			eval "___emu_a_${___arr_name}_${___key}=\"$___val\""
+			___keys="$___keys$___key$___emu_arr_delim"
+		done
+	fi
+
+	eval "___emu_a_${___arr_name}_keys=\"$___keys\""
+
+	unset ___val ___key ___keys
+	return 0
+}
+
+# get all values from an emulated associative array (unsorted)
+# 1 - array name
+# no additional arguments are allowed
+get_a_arr_all() {
+	[ $# -ne 1 ] && { echo "get_a_arr_all: Error: wrong number of arguments." >&2; return 1; }
+	___arr_name="$1"; ___all_keys=""
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "get_a_arr_all: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+
+	eval "___keys=\"$(printf '%s' "\$___emu_a_${___arr_name}_keys")\""
+	IFS_OLD="$IFS"
+	IFS="$___emu_arr_delim"
+	for ___key in $___keys; do
+		eval "___val=\"\$___emu_a_${___arr_name}_${___key}\""
+		[ -n "$___val" ] && { printf '%s\n' "$___val"; ___all_keys="$___all_keys$___key$___emu_arr_delim"; }
+	done
+	IFS="$IFS_OLD"
+	eval "___emu_a_${___arr_name}_keys=\"$___all_keys\""
+
+	unset ___keys ___key ___all_keys
 	return 0
 }
 
@@ -59,125 +183,48 @@ get_i_arr_all() {
 get_a_arr_all_keys() {
 	[ $# -ne 1 ] && { echo "get_a_arr_all_keys: Error: wrong number of arguments." >&2; return 1; }
 	___arr_name="$1"
-	eval "___pairs=\"\$emu_a_${___arr_name}\""
-	___pairs="${___pairs#"${___emu_arr_delim}"}"
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "get_a_arr_all_keys: Error: invalid array name '$___arr_name'." >&2; return 1; esac
 
-	# shellcheck disable=SC2154
+	eval "___keys=\"\$___emu_a_${___arr_name}_keys\""
 	___all_keys="$(
-		IFS_OLD="$IFS"
-		IFS="${___emu_arr_delim}"
-		for ___pair in $___pairs; do
-			printf '%s' "${___pair%%"${___pair_delim}"*} "
+		IFS="$___emu_arr_delim"
+		for ___key in $___keys; do
+			eval "___val=\"\$___emu_a_${___arr_name}_${___key}\""
+			printf '%s\n' "$___key"
 		done
-		IFS="$IFS_OLD"
 	)"
 
-	printf '%s\n' "${___all_keys% }"
-	unset ___all_keys ___arr_name ___pairs
+	eval "___emu_a_${___arr_name}_keys=\"$(printf '%s' "$___all_keys" | tr '\n' "$___emu_arr_delim")\""
+	printf '%s' "$___all_keys"
 
+	unset ___keys ___key ___all_keys
 	return 0
 }
 
-
-# set a value in an emulated indexed array
+# set a value in an emulated associative array
 # 1 - array name
-# 2 - index
-# 3 - value (if no value, unsets the index)
-# no additional arguments are allowed
-set_i_arr_el() {
-	if [ $# -lt 2 ] || [ $# -gt 3 ]; then echo "set_i_arr_el: Error: Wrong number of arguments." >&2; return 1; fi
-	___arr_name="$1"; ___index=$2; ___new_val="$3"
-
-	case $___index in ''|*[!0-9]*) echo "get_i_arr_el: Error: no index specified or '$___index' is not a nonnegative integer." >&2; return 1 ;; esac
-
-	eval "___pairs=\"\$emu_i_${___arr_name}\""
-
-	# this is ugly but massively faster than 'for' loop, grep, awk and sed solutions
-	case "$___pairs" in
-		*"${___emu_arr_delim}${___index}${___pair_delim}"* )
-			___pairs_pt1="${___pairs%%"${___emu_arr_delim}${___index}${___pair_delim}"*}"
-			___pairs_pt2="${___pairs#*"${___emu_arr_delim}${___index}${___pair_delim}"*"${___emu_arr_delim}"}"
-			___new_pairs="$___pairs_pt1${___emu_arr_delim}$___pairs_pt2"
-		;;
-		* ) ___new_pairs="$___pairs"; [ -z "$___new_pairs" ] && ___new_pairs="${___emu_arr_delim}"
-	esac
-
-	[ -n "$___new_val" ] && ___new_pairs="${___new_pairs}${___index}${___pair_delim}${___new_val}${___emu_arr_delim}"
-
-	eval "emu_i_$___arr_name=\"$___new_pairs\""
-
-	unset ___arr_name ___new_val ___new_pairs ___index ___pairs ___pairs_pt1 ___pairs_pt2
-	return 0
-}
-
-# set a key=value pair in an emulated associative array
-# 1st arg - array name
-# 2nd arg - 'key=value' pair
+# 2 - key
+# 3 - value (if no value, unsets the key)
 # no additional arguments are allowed
 set_a_arr_el() {
-	[ $# -ne 2 ] && { echo "set_a_arr_el: Error: wrong number of arguments." >&2; return 1; }
-	___arr_name="$1"; ___new_pair="$2"
+	___arr_name="$1"; ___pair="$2"
+	if [ $# -ne 2 ]; then echo "set_a_arr_el: Error: Wrong number of arguments." >&2; return 1; fi
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "set_a_arr_el: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+	case "$___pair" in *=*) ;; *) echo "set_a_arr_el: Error: '$___pair' is not a 'key=value' pair." >&2; return 1 ;; esac
+	___key="${___pair%%=*}"
+	___new_val="${___pair#*=}"
+	case "$___key" in *[!A-Za-z0-9_]*) echo "set_a_arr_el: Error: invalid key '$___key'." >&2; return 1; esac
 
-	case "$___new_pair" in *=*) ;; *) echo "set_a_arr_el: Error: '$___new_pair' is not a 'key=value' pair." >&2; return 1 ;; esac
-	___key="${___new_pair%%=*}"
-	___new_val="${___new_pair#*=}"
+	eval "___old_val=\"\$___emu_a_${___arr_name}_${___key}\""
 
-	[ -z "$___key" ] && { echo "set_a_arr_el: Error: empty value provided for key in input '$___new_pair'." >&2; return 1; }
+	if [ -z "$___old_val" ] && [ -n "$___new_val" ]; then
+		eval "___emu_a_${___arr_name}_keys=\"\${___emu_a_${___arr_name}_keys}${___key}${___emu_arr_delim}\""
+	fi
 
-	eval "___pairs=\"\$emu_a_${___arr_name}\""
+	eval "___emu_a_${___arr_name}_${___key}=\"$___new_val\""
 
-	# this is ugly but massively faster than 'for' loop, grep, awk and sed solutions
-	case "$___pairs" in
-		*"${___emu_arr_delim}${___key}${___pair_delim}"* )
-			___pairs_pt1="${___pairs%%"${___emu_arr_delim}${___key}${___pair_delim}"*}"
-			___pairs_pt2="${___pairs#*"${___emu_arr_delim}${___key}${___pair_delim}"*"${___emu_arr_delim}"}"
-			___new_pairs="$___pairs_pt1${___emu_arr_delim}$___pairs_pt2"
-		;;
-		* ) ___new_pairs="$___pairs"; [ -z "$___new_pairs" ] && ___new_pairs="${___emu_arr_delim}"
-	esac
-
-	___new_pairs="${___new_pairs}${___key}${___pair_delim}${___new_val}${___emu_arr_delim}"
-
-	eval "emu_a_$___arr_name=\"$___new_pairs\""
-
-	unset ___arr_name ___new_pair ___key ___new_val ___new_pairs ___pairs_pt1 ___pairs_pt2
+	unset ___arr_name ___key ___new_val ___keys ___old_val
 	return 0
-}
-
-
-# backend function which serves both types of arrays
-# 1 - array name
-# 2 - key/index
-___get_arr_el() {
-	___val=""; ___arr_name="$1"; ___arg_key="$2"
-
-	eval "___pairs=\"\$emu_${___arr_type}_${___arr_name}\""
-
-	IFS_OLD="$IFS"
-	IFS="${___emu_arr_delim}"
-	# shellcheck disable=SC2154
-	for ___pair in $___pairs; do
-		___key="${___pair%%"${___pair_delim}"*}"
-		[ "$___key" = "$___arg_key" ] && { ___val="${___pair#*"${___arg_key}${___pair_delim}"}"; break; }
-	done
-	IFS="$IFS_OLD"
-
-	printf '%s\n' "$___val"
-	unset ___val ___arr_name ___arg_key ___pairs ___pair ___key
-
-	return 0
-}
-
-# get a value from an emulated indexed array
-# 1 - array name
-# 2 - index
-# no additional arguments are allowed
-get_i_arr_el() {
-	[ $# -ne 2 ] && { echo "get_i_arr_el: Error: wrong number of arguments." >&2; return 1; }
-	___arr_type="i"
-
-	case "$2" in ''|*[!0-9]*) echo "get_i_arr_el: Error: no index specified or '$2' is not a nonnegative integer." >&2; return 1 ;; esac
-	___get_arr_el "$@"
 }
 
 # get a value from an emulated associative array
@@ -185,11 +232,30 @@ get_i_arr_el() {
 # 2 - key
 # no additional arguments are allowed
 get_a_arr_el() {
-	[ $# -ne 2 ] && { echo "get_a_arr_el: Error: wrong number of arguments." >&2; return 1; }
-	___arr_type="a"
+	if [ $# -ne 2 ]; then echo "get_a_arr_el: Error: Wrong number of arguments." >&2; return 1; fi
+	___arr_name="$1"; ___key=$2
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "get_a_arr_el: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+	case "$___key" in *[!A-Za-z0-9_]*) echo "get_a_arr_el: Error: invalid key '$___key'." >&2; return 1; esac
 
-	___get_arr_el "$@"
+	eval "printf '%s\n' \"\$___emu_a_${___arr_name}_${___key}\""
 }
+
+clean_a_arr() {
+	[ $# -ne 1 ] && { echo "clean_a_arr: Error: wrong number of arguments." >&2; return 1; }
+	___arr_name="$1"
+	case "$___arr_name" in *[!A-Za-z0-9_]*) echo "clean_a_arr: Error: invalid array name '$___arr_name'." >&2; return 1; esac
+	eval "___keys=\"\$___emu_a_${___arr_name}_keys\""
+
+	IFS_OLD="$IFS"
+	IFS="$___emu_arr_delim"
+	for ___key in $___keys; do
+		unset "___emu_a_${___arr_name}_${___key}"
+	done
+	IFS="$IFS_OLD"
+	unset "___emu_a_${___arr_name}_keys"
+	unset ___keys ___key
+}
+
 
 
 ___newline="
